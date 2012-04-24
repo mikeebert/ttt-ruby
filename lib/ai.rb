7e9448bac9acbc1f41c53cb367153774e3714224
mikeebert <mike@mikeebert.net>
2012-04-24 13:26:03 -0500

    Refactoring of minimax methods

--- lib/ai.rb
+++ lib/ai.rb
@@ -14,8 +14,7 @@ class Ai
   def get_minimax_move(board)
     set_min_and_max_players(board)
     @possible_moves = []
-    new_score = -100
-    best_score_for_max = -5    
+    best_score_for_max = -5
     
     board.available_spaces.each do |space|
       test_board = copy(board)
@@ -23,7 +22,7 @@ class Ai
       new_score = minimax_score(test_board)
       if new_score > best_score_for_max
         best_score_for_max = new_score
-        @possible_moves = [] #reset the @possible_moves if higher than others
+        @possible_moves = [] #reset the @possible_moves if ranked higher than others
         @possible_moves << space
       elsif new_score == best_score_for_max
         @possible_moves << space
@@ -36,57 +35,42 @@ class Ai
   def minimax_score(board)
     score = game_value(board)
     return score unless score == -1
-
-    if board.next_player == @min_player
-      scoring_for_player = @min_player
-      new_score = 100
-      best_score = 5
-    else
-      scoring_for_player = @max_player
-      new_score = -100
-      best_score = -5    
-    end
+    best_score = starting_score_for(board.next_player)
+    player = board.next_player
 
     board.available_spaces.each do |space|
       test_board = copy(board)
-
+      place_test_move(test_board,space)
       # test_board.place_move(board.next_player_symbol, space)
-
-      if test_board.next_player == @min_player
-        test_board.place_move(@min_symbol, space)
-      else
-        test_board.place_move(@max_symbol, space)
-      end
-
-      new_score = minimax_score(test_board)
-
-      best_score = compare_min(best_score,new_score) if scoring_for_player == @min_player
-      best_score = compare_max(best_score,new_score) if scoring_for_player == @max_player
-
+      new_score = minimax_score(test_board)      
+      best_score = compare(best_score, new_score, player)
     end
     return best_score
   end
   
-  def compare_min(best_score,new_score)
-    new_score < best_score ? new_score : best_score
+  def place_test_move(board,space)
+    if board.next_player == @min_player
+      board.place_move(@min_symbol, space)
+    else
+      board.place_move(@max_symbol, space)
+    end    
   end
   
-  def compare_max(best_score,new_score)
-    new_score > best_score ? new_score : best_score
+  def starting_score_for(player)
+    if player == @min_player 
+      return 5 
+    else
+      return -5
+    end
   end
   
-  # def set_scoring_for(board)
-  #   if board.next_player == @min_player
-  #     @scoring_for_player = @min_player
-  #     @new_score = 100
-  #     @best_score = 5
-  #   else
-  #     @scoring_for_player = @max_player
-  #     @new_score = -100
-  #     @best_score = -5    
-  #   end    
-  #   return {new_score: @new_score, best_score: @best_score, scoring: @scoring_for_player}
-  # end
+  def compare(best_score, new_score, player)
+    if player == @min_player
+      new_score < best_score ? new_score : best_score
+    else
+      new_score > best_score ? new_score : best_score
+    end
+  end
     
   def game_value(board)
     if board.has_winner || board.is_draw
