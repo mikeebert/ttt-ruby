01e44727872168bb7262c1842f355f7f6d186a31
mikeebert <mike@mikeebert.net>
2012-05-21 15:48:42 -0500

    Created LimelightUI class to use with a new game implementing the Limelight interface

--- lib/limelight_ui.rb
+++ lib/limelight_ui.rb
@@ -1,4 +1,65 @@
 class LimelightUI
+
+  attr_accessor :input
+  attr_accessor :play_again
+
+  def welcome_message
+  end
   
+  def display_board(board)
+  end
+
+  def get_details_for_player(n)
+  end
+
+  def prompt_for_type(n)
+  end
+
+  def prompt_for_symbol(n)
+  end
+
+  def prompt_for_board_size
+  end
+
+  def display_instructions
+  end
+
+  def invalid_move_message
+    puts "Not a valid move. Please enter another move:"
+  end
+
+  def computer_move_message
+    puts "Check out my move."
+  end
+
+  def winning_message(winner)
+    puts "#{winner} wins. Game Over."
+  end
+
+  def draw_message
+    puts "Tie Game. Game Over."
+  end
+
+  def prompt_for_next_move
+    puts "Enter your next move:"
+  end
+
+  def prompt_to_play_again
+    puts "Would you like to play again? (Y/N)"
+    @input = gets.chomp.downcase
+
+    if input == "n"
+      @play_again = :no
+    elsif input == "y"
+      @play_again = :yes
+    else    
+      puts "Sorry, I couldn't understand that. Enter a single Y or N for Yes or No."
+      prompt_to_play_again
+    end
+  end
+
+  def get_input 
+    @input = gets.chomp.to_i
+  end
   
 end
