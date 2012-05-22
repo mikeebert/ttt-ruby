7f2cdccf7c40c0f445fbe59c3ee259a85584afef
mikeebert <mike@mikeebert.net>
2012-05-22 09:48:32 -0500

    Requiring Board class and creating Board object on scene opening

--- limelight/default_scene/players/limelight_board.rb
+++ limelight/default_scene/players/limelight_board.rb
@@ -1,13 +1,15 @@
 $: << File.expand_path(File.dirname(__FILE__) + "/../../../lib")
 # print $: << File.expand_path(File.dirname(__FILE__) + "../../../..")
-require 'game'
+require 'board'
 require 'limelight_ui'
 
 module LimelightBoard
+  attr_accessor @board
   
   def scene_opened(event)
-    ui = LimelightUI.new
-    @game = Game.new(ui)
+    # ui = LimelightUI.new
+    #     @game = Game.new(ui)
+    @board = Board.new(3)
     production.player = 1
   end
   
