4cbc57e341f318583d02446680187f00d4c1c953
mikeebert <mike@mikeebert.net>
2012-05-23 08:52:42 -0500

    Added tests for start_button player

--- limelight/spec/default_scene/default_scene_spec.rb
+++ limelight/spec/default_scene/default_scene_spec.rb
@@ -4,6 +4,15 @@ describe "Default Scene" do
 
   uses_limelight :scene => "default_scene", :hidden => true
 
+  before(:each) do
+    start = scene.find("start_btn")
+    start.mouse_clicked(nil)
+  end
+
+  it "should create a board" do
+    production.board.should_not be_nil
+  end
+  
   it "should change the square when clicked" do
     square = scene.find("square1")
     square.mouse_clicked(nil)
@@ -15,11 +24,6 @@ describe "Default Scene" do
     square2 = scene.find("square2")
     square5 = scene.find("square5")
     test_squares = [square1, square2, square5]
-    # 
-    # test_squares.each do |square|
-    #   square.mouse_clicked(nil)
-    #   square.text.should_not == ""
-    # end
 
     reset_button = scene.find("reset_btn")
     reset_button.mouse_clicked(nil)
@@ -28,5 +32,12 @@ describe "Default Scene" do
       square.text.should == ""
     end
     
+  end
+  
+  it "should reset the production board" do
+    reset_button = scene.find("reset_btn")
+    reset_button.mouse_clicked(nil)
+    production.board.winner.should == nil
+    production.board.grid.should_not
   end
 end
