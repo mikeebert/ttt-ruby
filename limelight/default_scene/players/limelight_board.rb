dbe88424063c8b2b9ca851e61e8e3a15fbc7c544
mikeebert <mike@mikeebert.net>
2012-05-23 08:53:48 -0500

    Removed Board object attribute and moved it to Production

--- limelight/default_scene/players/limelight_board.rb
+++ limelight/default_scene/players/limelight_board.rb
@@ -2,15 +2,12 @@ $: << File.expand_path(File.dirname(__FILE__) + "/../../../lib")
 # print $: << File.expand_path(File.dirname(__FILE__) + "../../../..")
 require 'board'
 require 'limelight_ui'
-
+    
 module LimelightBoard
-  attr_accessor @board
   
   def scene_opened(event)
     # ui = LimelightUI.new
     #     @game = Game.new(ui)
-    @board = Board.new(3)
-    production.player = 1
   end
   
 end
