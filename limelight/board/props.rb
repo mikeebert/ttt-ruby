3d6d558a5844337c5d3fbd41e0d46c8c4697b726
mikeebert <mike@mikeebert.net>
2012-05-25 16:10:15 -0500

    added info_panel to display game messages

--- limelight/board/props.rb
+++ limelight/board/props.rb
@@ -3,11 +3,20 @@
 #
 # For more information see: http://limelightwiki.8thlight.com/index.php/A_Cook%27s_Tour_of_Limelight#PropBuilder_DSL
 
-control_row do
-  start_button :id => "start_btn", :text => "START", :styles => "button", :on_mouse_clicked => "scene.begin_game" # do
-  reset_button :id => "reset_btn", :styles => "button", :text => "RESET"
-end
-
 limelight_board :id => "limelight_board" do
-  (1..9).each { |n| square :id => "square#{n}", :styles => "square"}
+  
+  control_row do
+    option_button :text => "Human vs Human", :styles => "button", :on_mouse_clicked => "scene.begin_game"
+    option_button :text => "Human vs Computer", :styles => "button", :on_mouse_clicked => "scene.begin_game"
+    reset_button :id => "reset_btn", :styles => "button", :text => "RESET"
+  end
+
+  ttt_board do
+    (1..9).each { |n| square :id => "square#{n}", :styles => "square"}
+  end
+  
+  info_panel do
+    display_text :id => 'display_text', :text => "Welcome to Tic Tac Toe."
+  end
+
 end
