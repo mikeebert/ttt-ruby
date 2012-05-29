d0b79a7e5160f4b7326df98a455f09b9a888d1e4
mikeebert <mike@mikeebert.net>
2012-05-29 17:13:57 -0500

    added method to set production.human_move to a clicked on square

--- limelight/board/players/square.rb
+++ limelight/board/players/square.rb
@@ -3,9 +3,7 @@ module Square
   def mouse_clicked(event)    
     if available?
       n = self.id.match(/\d/)[0]
-      symbol = production.game.board.next_player_symbol
-      production.game.update_board(symbol,n.to_i)
-      self.text = symbol
+      production.human_move = n.to_i
     end
   end
   
