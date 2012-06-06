73fe9fc6f9524784bc310c770d25e6a4efe90263
mikeebert <mike@mikeebert.net>
2012-06-06 15:03:31 -0500

    updated tests for instantiating a player

--- spec/player_factory_spec.rb
+++ spec/player_factory_spec.rb
@@ -1,11 +1,16 @@
 require 'player_factory'
 
 module TTT
-  describe PlayerFactory do  
+  describe PlayerFactory do
+    
+    before(:each) do
+      @player_factory = PlayerFactory.new
+    end
+    
     it "should create a human player if the type is human" do
       input = {type: :human, symbol: "X"}
       ui = :some_ui
-      player = PlayerFactory.create(input, ui)
+      player = @player_factory.create(input, ui)
       player.should be_an_instance_of(HumanPlayer)
       player.symbol.should == "X"
       player.ui.should == :some_ui
@@ -14,7 +19,7 @@ module TTT
     it "should create a computer" do
       input = {type: :computer, symbol: "O"}
       ui = :some_ui
-      player = PlayerFactory.create(input, ui)
+      player = @player_factory.create(input, ui)
       player.should be_an_instance_of(ComputerPlayer)
       player.symbol.should == "O"
     end
