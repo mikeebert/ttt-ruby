94b2f3d9600c595b649e0806ec8d33a63f966970
mikeebert <mike@mikeebert.net>
2012-06-06 09:23:44 -0500

    Built out the Game setup to set the board and players. Still includes PlayerFactory

--- lib/cli_play.rb
+++ lib/cli_play.rb
@@ -1,13 +1,49 @@
 require 'game'
+require 'player_factory'
 
 class CliPlay
   
-  attr_accessor :game, :ui
+  attr_accessor :game, :ui, :player_factory, :player1, :player2
   
-  def initialize(ui)
-    @game = TTT::Game.new
+  def initialize(ui)    
     @ui = ui
+    @game = TTT::Game.new
+    @player_factory = TTT::PlayerFactory.new
   end
   
+  def setup_game
+    @ui.welcome_message
+    set_players
+    setup_game_board
+    play_game unless exit_game? #exit_game? really just for testing. Should get rid of it
+  end
   
+  def play_game
+    @ui.display_board(@game.current_board)
+    next_player_move 
+    play_game unless game.is_over?
+  end
+  
+  def next_player_move
+    
+  end
+    
+  def set_players
+    set_player(1)
+    set_player(2)
+  end
+
+  def set_player(n)
+    input = @ui.get_details_for_player(n)
+    player = @player_factory.create(input, @ui)
+    n == 1 ? @player1 = player : @player2 = player
+  end
+    
+  def setup_game_board
+    @game.set_board_symbols(@player1.symbol,@player2.symbol)    
+  end
+  
+  def exit_game?
+    @ui.play_again == :no
+  end
 end
--- spec/cli_play_spec.rb
+++ spec/cli_play_spec.rb
@@ -1,18 +1,69 @@
 require 'cli_play'
 require 'mocks/mock_game'
 require 'mocks/mock_ui'
+require 'mocks/mock_player_factory'
+require 'mocks/mock_board'
 
 
 describe CliPlay do
   before(:each) do
     @ui = FakeUI.new
-  end
-  
-  it "should create a new Game" do
     @cli_runner = CliPlay.new(@ui)
-    @cli_runner.game = FakeGame.new
-    @cli_runner.game.should be_an_instance_of(FakeGame)
+    @game = FakeGame.new
+    @cli_runner.game = @game
+    @player_factory = MockPlayerFactory.new
+    @cli_runner.player_factory = @player_factory
+    @ui.player_details = [{type: :human, symbol: "X"},
+                          {type: :computer, symbol: "O"}]
   end
-  
-  
+
+  describe "setting up the game" do
+    before(:each) do
+      @ui.play_again == :no
+    end
+
+    it "should create a new Game" do
+      @cli_runner.ui.should be_an_instance_of(FakeUI)
+      @cli_runner.game.should be_an_instance_of(FakeGame)
+    end
+
+
+    it "should welcome the competitors" do
+      @cli_runner.setup_game
+      @ui.welcomed_competitors.should == true
+    end
+
+    it "should ask the ui to provide the type and symbol for a player" do
+      @cli_runner.set_players
+      @ui.requested_player_details.should == true
+    end    
+
+    it "should pass the player details from the ui to the player factory" do
+      @ui.player_details = [{type: :human, symbol: "X"},
+                            {type: :computer, symbol: "O"}]
+      @cli_runner.set_players
+      @player_factory.player1_type.should == :human
+      @player_factory.player1_symbol.should == "X"
+      @player_factory.player2_type.should == :computer
+      @player_factory.player2_symbol.should == "O"
+    end
+
+    it "should send the player symbols to the game for the board" do
+      @cli_runner.setup_game
+      @game.board_player1_symbol.should == "X"
+      @game.board_player2_symbol.should == "O"
+    end
+  end
+    
+  describe "Playing the Game" do
+    it "should pass the Game's current board to the ui to display" do
+      @game.board = :some_board
+      @cli_runner.play_game
+      @ui.displayed_board.should == :some_board
+    end
+    
+    it "should " do
+      
+    end
+  end
 end
