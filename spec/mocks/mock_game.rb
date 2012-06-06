6835d232241648a0ea87be9704ae54e46cae606e
mikeebert <mike@mikeebert.net>
2012-06-06 15:03:05 -0500

    Added in more methods for testing play_cli class

--- spec/mocks/mock_game.rb
+++ spec/mocks/mock_game.rb
@@ -1,10 +1,16 @@
 class FakeGame
   
   attr_accessor :board,
+                :board_next_player,
                 :board_player1_symbol,
                 :board_player2_symbol,
                 :created_board,
-                :over  
+                :moves_made,
+                :over,
+                :over_values,
+                :provided_next_player,
+                :received_move_space,
+                :received_move_symbol
   
   def create_board(size)
     @created_board = true
@@ -16,7 +22,19 @@ class FakeGame
   end
   
   def current_board
-    @board    
+    @over = @over_values.shift if @over_values != nil
+    return @board    
+  end
+  
+  def next_player
+    @provided_next_player = true
+    return @board_next_player
+  end
+  
+  def make_move(symbol,move)
+    @moves_made += 1 if @moves_made != nil
+    @received_move_symbol = symbol
+    @received_move_space = move
   end
   
   def is_over?
