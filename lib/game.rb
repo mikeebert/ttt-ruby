2058a4f70bc3178d26cdec5af9400697248634a7
mikeebert <mike@mikeebert.net>
2012-06-06 14:57:37 -0500

    deleted more methods that are now in play_cli class

--- lib/game.rb
+++ lib/game.rb
@@ -9,33 +9,16 @@ module TTT
     def initialize
       @board = Board.new
     end
-      
-    # def play
-    #   @ui.welcome_message
-    #   set_competitors
-    #   @ui.display_instructions
-    #   play_script unless exit_game 
-    # end
-  
-    def play_script
-      @ui.display_board(@board.moves)
-      next_player_move
-      if is_over?
-        game_over_scenario 
-      else
-        play_script
-      end
-    end
     
     def set_board_symbols(symbol1,symbol2)
       @board.player1_symbol = symbol1
       @board.player2_symbol = symbol2
     end
-  
-    def next_player_move
-      @board.next_player == :player1 ? @player1.move(@board) : @player2.move(@board)
+    
+    def next_player
+      @board.next_player
     end
-  
+    
     def game_over_scenario
       game_over_message
       @ui.display_board(@board.moves)
@@ -62,10 +45,6 @@ module TTT
   
     def ask_to_play_again
       @ui.prompt_to_play_again
-    end
-  
-    def exit_game
-      @ui.play_again == :no
     end
   end
 end
