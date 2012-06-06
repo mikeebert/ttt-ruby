eef294f87b35c7a25d7d82f49dd55e2c86498af1
mikeebert <mike@mikeebert.net>
2012-06-06 09:27:36 -0500

    created a mock_game for testing with cli_play

--- spec/mocks/mock_game.rb
+++ spec/mocks/mock_game.rb
@@ -1,3 +1,25 @@
 class FakeGame
   
+  attr_accessor :board,
+                :board_player1_symbol,
+                :board_player2_symbol,
+                :created_board,
+                :over  
+  
+  def create_board(size)
+    @created_board = true
+  end
+  
+  def set_board_symbols(symbol1,symbol2)
+    @board_player1_symbol = symbol1
+    @board_player2_symbol = symbol2
+  end
+  
+  def current_board
+    @board    
+  end
+  
+  def is_over?
+    return @over
+  end
 end
