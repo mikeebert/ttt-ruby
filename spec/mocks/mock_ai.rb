dc4ad51d08a395cd9112e7e4bc4fb7e0a457cd81
mikeebert <mike@mikeebert.net>
2012-06-07 14:52:39 -0500

    deleted unnecessary move method that's no longer used

--- spec/mocks/mock_ai.rb
+++ spec/mocks/mock_ai.rb
@@ -2,11 +2,6 @@ class FakeAi
 
   attr_accessor :received_board, :move
   
-  def move(board)
-    @received_board = true
-    return @move
-  end
-  
   def random_move(board)
     @received_board = true
     return @move
