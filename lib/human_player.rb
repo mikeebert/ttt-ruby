3e9a55f2c23efb86639af2c456d13e7f6e7d6cee
mikeebert <mike@mikeebert.net>
2012-06-07 09:27:59 -0500

    Cleaned up HumanPlayer to only include symbol and human? method

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -5,22 +5,9 @@ module TTT
     def initialize(symbol)
       @symbol = symbol
     end
-  
-    def get_move(board)
-      @ui.prompt_for_next_move
-      sleep 0.1
-      move = @ui.get_input
-      if board.valid_move(move)
-        return move
-      else      
-        @ui.invalid_move_message
-        get_move(board)
-      end
-    end  
-
-    def move(board)
-      move = self.get_move(board)
-      board.place_move(@symbol, move)
+    
+    def human?
+      true
     end
   end
 end
--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -6,37 +6,15 @@ require 'mocks/mock_board'
 module TTT
   describe HumanPlayer do  
     before(:each) do
-      @ui = FakeUI.new
       @human = HumanPlayer.new("x")
-      @human.ui = @ui
-      @ui.input_values = [:move_prompt_stub, :valid_move]
-      @board = FakeBoard.new
-    end
-  
-    it "should respond to the move method from the Player superclass" do
-      @human.move(@board)
-      @ui.prompted_user.should == :next_move_please
-    end
-  
-    it "should send the move and it's symbol to a board via the Player move method" do
-      @human.move(@board)
-      @board.received_symbol.should == ("x")
-      @board.received_move.should == :valid_move
-    end
-  
-    it "should prompt the human for the next move" do
-      @human.get_move(@board)
-      @ui.prompted_user.should == :next_move_please
-    end
-  
-    it "should return a move that it gets from the UI if valid" do
-      @human.get_move(@board).should == :valid_move
     end
 
-    it "should request a move again from the UI if the move isn't valid" do
-      @ui.input_values = [:move_prompt_stub, :invalid_move,:move_prompt_stub, :valid_move]
-      @human.get_move(@board)
-      @ui.prompted_counter.should == 2
+    it "should have a symbol" do
+      @human.symbol.should == "x"
+    end
+    
+    it "should return true if asked if it's a human player" do
+      @human.human?.should == true
     end
   end
 end
