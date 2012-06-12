5614b8a99496d61c02613d2d90b8febda6915f16
mikeebert <mike@mikeebert.net>
2012-06-12 15:03:24 -0500

    Refactor and testing of Limelight_game class

--- lib/limelight_game.rb
+++ lib/limelight_game.rb
@@ -5,52 +5,47 @@ class LimelightGame
   attr_accessor :game, :ui
   attr_accessor :player_factory, :player1, :player2
 
-  def initialize(ui, player1, player2)
+  def initialize(ui)
     @ui = ui
     @game = TTT::Game.new
     @player_factory = TTT::PlayerFactory.new
-    create_players(player1, player2)
-    setup_game_board
   end
   
   def create_players(player1,player2)
     @player1 = @player_factory.create(player1)
     @player2 = @player_factory.create(player2)
+    set_game_symbols
   end
   
-  def setup_game_board
+  def set_game_symbols
     @game.set_game_player_symbols(@player1.symbol,@player2.symbol)
   end
   
-  def begin
+  def begin_game
     @ui.display_game_in_progress
-    check_for_computer_move    
+    get_computer_move if next_player.human? == false
   end
   
-  def update_game(n)
-    @game.make_move(@game.next_player_symbol,n)
+  def update_game(move)
+    @game.make_move(@game.next_player_symbol,move) #seems like a mess 
     @ui.display_board(@game.current_board)
     if @game.is_over?
       game_over_message
     else
-      check_for_computer_move
+      get_computer_move if next_player.human? == false
     end
   end
-
+  
+  def get_computer_move
+    move = next_player.get_ai_move(@game.board, 
+                                   @game.next_player_symbol,
+                                   @game.opponent_symbol)
+    sleep 0.3
+    update_game(move)
+  end
+  
   def next_player
     @game.next_player == :player1 ? @player1 : @player2
-  end
-  
-  def is_computer?
-    next_player.human? == false
-  end
-  
-  def check_for_computer_move
-    if next_player.human? == false
-      move = next_player.get_ai_move(@game.board, next_player.symbol, @game.opponent_symbol)
-      sleep 0.3
-      update_game(move)
-    end
   end
   
   def game_over_message
@@ -63,6 +58,8 @@ class LimelightGame
   
   def reset_game
     @game.reset
+    @ui.clear_board
+    @ui.prompt_for_new_game
   end      
   
 end
--- spec/limelight_game_spec.rb
+++ spec/limelight_game_spec.rb
@@ -1,28 +1,90 @@
 require 'limelight_game'
-require 'human_player'
-require 'player_factory'
+require 'mocks/mock_player_factory'
 require 'mocks/mock_game'
 require 'mocks/mock_ui'
-require 'mocks/mock_board'
 require 'mocks/mock_human'
 require 'mocks/mock_computer'
 
+
 module TTT
   describe LimelightGame do
-  before(:each) do
-    @ui = FakeUI.new
-    @game = FakeGame.new
-    @player_factory = TTT::PlayerFactory.new
-    @player1 = {type: :human, symbol: "X"}
-    @player2 = {type: :computer, symbol: "O"}
-    @limelight_game = LimelightGame.new(@ui,@player1,@player2)
-    @limelight_game.game = @game
-    @limelight_game.player_factory = @player_factory
-  end
-
-    it "should set up the players" do
-      @limelight_game.player1.class.should == TTT::HumanPlayer
-      @limelight_game.player2.class.should == TTT::ComputerPlayer
+    before(:each) do
+      @ui = FakeUI.new
+      @limelight_game = LimelightGame.new(@ui)    
+      @game = FakeGame.new
+      @limelight_game.game = @game
+      @player_factory = MockPlayerFactory.new
+      @limelight_game.player_factory = @player_factory
+      player1_details = {type: :human, symbol: "X"}
+      player2_details = {type: :computer, symbol: "O"}
+      @limelight_game.create_players(player1_details,player2_details)
+      @player1 = @limelight_game.player1
+      @player2 = @limelight_game.player2
     end
+
+    it "should send the details to the factory to set up the players" do
+      @player_factory.player1_type.should == :human
+      @player_factory.player1_symbol.should == "X"
+      @player_factory.player2_type.should == :computer
+      @player_factory.player2_symbol.should == "O"
+    end
+    
+    it "should send the player symbols to the game" do
+      @game.board_player1_symbol.should == "X"
+      @game.board_player2_symbol.should == "O"
+    end
+
+    describe "#begin_game" do
+      before(:each) do
+        @game.over = true
+      end
+      
+      it "should tell the UI to display a welcome message" do        
+        @limelight_game.begin_game
+        @ui.displayed_in_progress.should == true
+      end
+      
+      it "should get a move from the computer if next player is computer" do
+        @game.next_player = :player2
+        @limelight_game.begin_game
+        @player2.got_computer_move.should == true
+      end
+    end
+    
+    describe "#update_game" do
+      before(:each) do
+        @game.over = true
+      end
+      
+      it "should send a move to the game" do
+        @game.next_player = :player1
+        @limelight_game.update_game(:some_move)
+        @game.received_move_space.should == :some_move
+      end
+      
+      it "should tell the ui to display the board" do
+        @game.board = :some_board
+        @limelight_game.update_game(:some_move)
+        @ui.displayed_board.should == :some_board
+      end
+      
+      it "should check if the game is over" do
+        @limelight_game.update_game(:some_move)
+        @game.checked_status.should == true
+      end
+      
+      it "should send a winning message if there's a winner" do
+        @game.board_winning_symbol = :some_winner
+        @limelight_game.update_game(:some_move)
+        @ui.displayed_winning_message.should == true
+      end
+      
+      it "should send a draw message if there's a draw" do
+        @limelight_game.update_game(:some_move)
+        @ui.displayed_draw_message.should == true
+      end
+    end
+    
+    
   end
 end
