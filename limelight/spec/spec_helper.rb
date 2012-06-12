00165b88b756dbce52ef77185bd59d3b0772f3ce
mikeebert <mike@mikeebert.net>
2012-06-12 15:06:20 -0500

    updated spec_helper to include mocks

--- limelight/spec/spec_helper.rb
+++ limelight/spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'spec'
 require 'limelight/specs/spec_helper'
+# require 'limelight/mouse'
 
 # Set the Gem home to the frozen gems
 $PRODUCTION_PATH = File.expand_path(File.dirname(__FILE__) + "/../")
@@ -12,3 +13,5 @@ Dir.glob(File.join("__resources", "gems", "gems", "**", "lib")).each do |dir|
 end
 
 $: << File.expand_path(File.dirname(__FILE__) + "/../../lib")
+$: << File.expand_path(File.dirname(__FILE__) + "/../../spec")
+$: << File.expand_path(File.dirname(__FILE__) + "/../../spec/mocks")
