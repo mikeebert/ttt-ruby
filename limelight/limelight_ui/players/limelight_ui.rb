66f697d78ed319eb0f4e4d1779c2ba767fefbeb8
mikeebert <mike@mikeebert.net>
2012-06-12 15:08:04 -0500

    removed responsibilities of limelight_ui scene to act on game. it only receives messages now

--- limelight/limelight_ui/players/limelight_ui.rb
+++ limelight/limelight_ui/players/limelight_ui.rb
@@ -1,18 +1,5 @@
 module LimelightUi
 
-  def start_game(player1,player2)
-    production.game = LimelightGame.new(self, player1, player2)
-    production.game.begin
-  end
-
-  def make_move(space)
-    production.game.update_game(space)
-  end
-
-  def get_details_for_player(n)
-    production.player_values.shift
-  end
-
   def display_board(moves)
     index = 0
     (1..9).to_a.each do |n|
@@ -21,7 +8,18 @@ module LimelightUi
       index += 1
     end
   end
+  
+  def clear_board
+    (1..9).each do |n|
+      scene.find("square#{n}").text = ""
+    end
+  end
 
+  def update_display(string)
+    scene.find("display").text = ""
+    scene.find("display").text = string
+  end
+  
   def winning_message(winner)
     update_display("Game Over. #{winner} Wins!")
   end
@@ -29,27 +27,12 @@ module LimelightUi
   def draw_message
     update_display("Game Over. Tie Game.")
   end
-  
-  def update_display(string)
-    scene.find("display").text = " "
-    scene.find("display").text = string
-  end
-  
+    
   def display_game_in_progress
     update_display("Game in Progress")
   end
   
-  def reset
-    clear_board 
-    production.game.reset_game
+  def prompt_for_new_game
     update_display("Press Start to Begin Game")
   end
-
-  def clear_board
-    (1..9).each do |n|
-      scene.find("square#{n}").text = ""
-    end
-  end
-    
-  
 end
