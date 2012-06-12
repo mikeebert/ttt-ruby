e4e54a482141ed275348d5a6585b7a3399095639
mikeebert <mike@mikeebert.net>
2012-06-12 15:36:06 -0500

    added method for reset_game

--- spec/mocks/mock_limelight_game.rb
+++ spec/mocks/mock_limelight_game.rb
@@ -1,8 +1,12 @@
 class MockLimelightGame
 
-  attr_accessor :received_move
+  attr_accessor :game_reset, :received_move
   
   def update_game(n)
     @received_move = true
+  end
+  
+  def reset_game
+    @game_reset = true
   end
 end
